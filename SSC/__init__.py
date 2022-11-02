3c93d0307e9ec4405e9147eb2ab5838e79a39d98
Edu Pagotto <edupagotto@gmail.com>
2022-11-02 20:13:50 -0300

    versao

--- SSC/__init__.py
+++ SSC/__init__.py
@@ -1,8 +1,8 @@
 '''
 Created on 20221007
-Update on 20221029
+Update on 20221102
 @author: Eduardo Pagotto
 '''
 
-__version__ : str = '1.1.0'
-__date_deploy__ : str = '20221029'
+__version__ : str = '1.2.0'
+__date_deploy__ : str = '20221102'
