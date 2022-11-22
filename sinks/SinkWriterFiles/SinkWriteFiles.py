7dfb66e65417afe4770c4327b590f1db2a99f1ea
Eduardo Pagotto <eduardo.pagotto@newspace.com.br>
2022-11-21 23:06:28 -0300

    sinkwhite

--- sinks/SinkWriterFiles/SinkWriteFiles.py
+++ sinks/SinkWriterFiles/SinkWriteFiles.py
@@ -4,12 +4,14 @@ Update on 20221121
 @author: Eduardo Pagotto
 '''
 
+import logging
 import pathlib
-import json
 from tinydb.table import Document
 
 from SSC.Message import Message
 from SSC.Sink import Sink
+
+from SSF.ClientSSF import ClientSSF
 
 class SinkWriterFiles(Sink):
     def __init__(self) -> None:
@@ -19,10 +21,14 @@ class SinkWriterFiles(Sink):
         self.others = pathlib.Path('./others')
         self.delay = 5
         self.watermark = 5
+        self.ssf : ClientSSF | None = None
+        self.log = logging.getLogger('SinkWriterFiles')
 
     def start(self, doc : Document) -> int:
         self.doc = doc
         self.config = doc['config']['configs']
+
+        self.log.info(f'{doc["name"]} start ')
 
         self.output = pathlib.Path(self.doc['storage'] + '/' + self.doc['config']['configs']['output'])
         self.output.mkdir(parents=True, exist_ok=True)
@@ -36,24 +42,44 @@ class SinkWriterFiles(Sink):
         self.delay = self.config['delay'] if 'delay' in self.config else 5
         self.watermark = self.config['watermark'] if 'watermark' in self.config else 2
 
+        self.ssf = ClientSSF(self.config['ssf_url']) 
+
         return self.delay
 
     def process(self, content : Message ) -> None:
         
-        #datetime.today().strftime('%Y%m%d')
+        topic_name = content.topic_name()
+        prop = content.properties()
+        if 'erro' not in topic_name:
+            if prop['valid'] is True:
+                src = pathlib.Path(prop['dst'])
+                dst = str(self.output.resolve()) + '/' +  prop['file']
+                src.replace(dst)
+                self.log.info(f'{self.doc["name"]} save {dst}')
 
-        payload =json.loads(content.data())
-        my_dict = payload['dados']
+                for extra in prop['extras']:
+                    id = extra['id']
+                    if id > 0:
+                        if self.ssf:
+                            file_prop = self.ssf.info(id)
+                            file = pathlib.Path(self.others, file_prop)
+                            valid, msg = self.ssf.download(id, str(file.resolve()))
+                            if not valid:
+                                raise Exception(msg)
+                            else:
+                                self.log.info(f'{self.doc["name"]} save extra {msg}')
 
-        # path = Path(self.file)
-        # if path.is_file():
-        #     with open(self.file, 'a') as f:
-        #         w = csv.writer(f)
-        #         w.writerow(my_dict.values())
-        # else:
-        #     with open(self.file, 'w') as f:
-        #         w = csv.writer(f)
-        #         w.writerow(my_dict.keys())
-        #         w.writerow(my_dict.values()) 
+                        else:
+                            self.log.error(f'Critico falta cfg de ssf no arguivo de configuracao')
+                    else:
+                        self.log.error(f'Extra invalido {extra["erro"]}')
 
-    
+        else:
+            if prop['valid'] is True:
+                # valido mas com erro
+                src = pathlib.Path(prop['dst'])
+                dst = str(self.erro.resolve()) + '/' +  prop['file']
+                src.replace(dst)
+                self.log.warning(f'{self.doc["name"]} save fail {dst}')
+            else:
+                self.log.error(f'{self.doc["name"]} registro invalido {prop["dst"]}')
