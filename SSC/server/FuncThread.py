b22bad05548eb020de856b9ac807e954c4fa0c94
Edu Pagotto <edupagotto@gmail.com>
2023-03-14 22:06:24 -0300

    simplificacao

--- SSC/server/FuncThread.py
+++ SSC/server/FuncThread.py
@@ -7,8 +7,10 @@ Update on 20230314
 import pathlib
 import time
 
-from typing import Optional
-
+import logging
+import importlib
+from typing import Any, Optional
+import threading
 from tinydb.table import Document
 from queue import Empty
 
@@ -16,17 +18,24 @@ from SSC.server.Namespace import Namespace
 from SSC.Context import Context
 from SSC.Message import Message
 from SSC.Function import Function
-from SSC.server.EntThread import EntThread
 from SSC.server.QueueProdCons import QueueConsumer, QueueProducer
 
-class FuncThread(EntThread):
+from SSC.server import EstatData
+
+class FuncThread(threading.Thread):
     def __init__(self, sufix : str, index : int, params : Document, namespace : Namespace) -> None:
 
-        super().__init__(sufix, index, params)
-
-        self.ns : Namespace = namespace
-
+        super().__init__(None, None, f'{sufix}_{index}_' + params['name'])
+        #super().__init__(sufix, index, params)
         self.map_producer : dict[str, QueueProducer] = {}
+        self.esta = EstatData()
+        self.sufix = sufix
+        self.ns : Namespace = namespace
+        self.sleep_read : float = float(params['sleep_read']) if 'sleep_read' in params else 0.0 # 5 segundos default
+        self.params = params
+        self.is_running = True
+    
+        self.log = logging.getLogger('SSC.EntThread')
 
         self.consumer : Optional[QueueConsumer] = None
         if ('inputs' in params) and (params['inputs'] is not None):
@@ -88,3 +97,47 @@ class FuncThread(EntThread):
                 time.sleep(5.0)
 
         self.log.info(f'{self.name} stopped')
+
+    def is_paused(self) -> bool:
+        if self.esta.pause is True:
+
+            if self.is_running is True:
+                self.log.info(f'{self.name} pause')
+                self.is_running = False
+
+            return True
+        else:
+            if self.is_running is False:
+                self.log.info(f'{self.name} resume')
+                self.is_running = True
+
+        return False
+
+    def load(self, path_file : pathlib.Path, class_name : str) -> Any:
+            klass = None
+
+            plugin = str(path_file.parent).replace('/','.') + '.' + class_name
+
+            self.log.info(f'{self.name} import {plugin}')
+
+            if plugin is None or plugin == '':
+                self.log.error("Cannot have an empty plugin string.")
+
+            try:
+                (module, x, classname) = plugin.rpartition('.')
+
+                if module == '':
+                    raise Exception()
+                mod = importlib.import_module(module)
+                klass = getattr(mod, classname)
+
+            except Exception as ex:
+                msg_erro = f"{self.name} could not enable class %s - %s" % (plugin, str(ex))
+                self.log.error(msg_erro)
+                raise Exception(msg_erro)
+
+            if klass is None:
+                self.log.error(f"{self.name} could not enable at least one class: {plugin}")
+                raise Exception(f"{self.name} could not enable at least one class: {plugin}") 
+
+            return klass()   
--- SSC/server/FunctionCrt.py
+++ SSC/server/FunctionCrt.py
@@ -4,15 +4,25 @@ Update on 20230313
 @author: Eduardo Pagotto
 '''
 
-from SSC.server.Namespace import Namespace
-from SSC.server.EnttCrt import EnttCrt
+import logging
+
+from threading import Lock
+from typing import Any, List, Tuple
+
+from SSC.server.Namespace import Namespace, splitQueue
 from SSC.server.FuncCocoon import FuncCocoon
 from SSC.subsys.LockDB import LockDB
 
-class FunctionCrt(EnttCrt):
+class FunctionCrt(object):
 
     def __init__(self, sufix : str, namespace : Namespace) -> None:
-        super().__init__(sufix, namespace)
+
+        self.colection_name = sufix
+        self.ns = namespace
+        self.log = logging.getLogger('SSC.EnttCrt')
+        self.lock_func = Lock()
+        self.list_entt : List[FuncCocoon] = []
+
         self.load_funcs_db()
 
     def create(self, params : dict) -> str:
@@ -46,3 +56,87 @@ class FunctionCrt(EnttCrt):
             except:
                 pass
 
+    def summario(self) ->dict:
+        
+        result = []
+        tot = 0
+        with self.lock_func:
+            tot = len(self.list_entt)  
+            for i in self.list_entt:
+                result.append(i.sumary())  
+
+        return {'online': tot, 'tasks': result}
+
+    def stop_func_all(self):
+
+        with self.lock_func:
+            for source in self.list_entt:
+                source.stop()
+
+            for source in self.list_entt:
+                source.join()
+                self.list_entt.remove(source)
+
+    def pause_resume(self, func_name : str, is_pause : bool):
+        namespace, name = splitQueue(func_name)   
+        with self.lock_func:
+            for fun in self.list_entt:
+                if ((fun.document['namespace'] == namespace) and (fun.document['name'] == name)):
+                    msg : str = ''
+                    if is_pause:
+                        msg = f'{name} paused'
+                        fun.pause()
+                    else:
+                        msg = f'{name} resumed'
+                        fun.resume()
+
+                    self.log.info(msg)
+                    return msg
+
+        raise Exception(f'{func_name} does not exist')
+
+    def delete(self, func_name : str):
+
+        self.log.debug(f'delete {func_name}')
+
+        namespace, name = splitQueue(func_name)
+        funcValid = None
+
+        with self.lock_func:
+            for source in self.list_entt:
+                if (namespace == source.document['namespace']) and (name == source.document['name']):
+                    self.list_entt.remove(source)
+                    funcValid = source
+
+        if funcValid:            
+            funcValid.stop()
+            funcValid.join()
+            with LockDB(self.ns.database, self.colection_name, True) as table:
+                table.remove(doc_ids=[funcValid.document.doc_id])
+
+            return f'success delete {func_name}'
+
+        raise Exception(f'{func_name} does not exist')
+
+    def list_all(self, ns : str) -> List[str]:
+        with LockDB(self.ns.database, self.colection_name, False) as table:
+            itens = table.all()
+
+        lista : List[str] = []
+        for item in itens:
+            if (ns == item['namespace']): 
+                lista.append(item['name'])
+
+        return lista
+
+    def execute(self) -> Tuple[int, int]:
+
+        tot_proc = 0
+        tot_erro = 0
+        with self.lock_func:
+            for source in self.list_entt:
+                ok, erro = source.count_tot()
+                tot_proc += ok
+                tot_erro += erro
+
+        return tot_proc, tot_erro
