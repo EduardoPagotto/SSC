a702e3a297164bf4bb81f9f1e87594b39f4e636d
Edu Pagotto <edupagotto@gmail.com>
2023-03-17 19:30:35 -0300

    simplificacao

--- builtin/FuncRelay.py
+++ builtin/FuncRelay.py
@@ -4,6 +4,10 @@ Update on 20230316
 @author: Eduardo Pagotto
 '''
 
+import logging
+
+from tinydb.table import Document
+
 from SSC.Function import Function
 from SSC.Context import Context
 
@@ -17,4 +21,30 @@ class FuncRelay(Function):
         #context.publish('test/ns01/queue04', input, context.get_message_properties(), context.get_message_key(), context.get_message_id())
         context.publish(context.get_output_queue(), input, context.get_message_properties(), context.get_message_key(), context.get_message_id())
         return 1
-    
+
+
+class SrcDummy(Function):
+    def __init__(self) -> None:
+        print('Dummy Constructor')
+        self.document : Document = Document({}, 0)
+        self.count = 0
+        self.serial = 0
+        self.water_mark = 2
+        self.log = logging.getLogger('Dummy')
+        super().__init__()
+
+    def process(self, input : str, context : Context) -> int:
+        
+        if context.get_producer_size(context.get_output_queue()) <= self.water_mark:
+
+            self.count += 1
+            if self.count % 2 == 0:
+                self.serial += 1
+                payload = f'msg {self.serial}'
+                #payload = {'nome':'eduardo', 'sexo' : True, 'serial':f'msg {self.serial}'}
+                self.log.debug(payload)
+
+                context.publish(context.get_output_queue(), payload, {}, '', self.count)
+                return 1
+        
+        return 0
